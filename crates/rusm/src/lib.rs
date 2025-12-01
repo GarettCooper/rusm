2298f81633fb8c66f63e6aa68669965e0cf99697
Garett Cooper <garett@garettcooper.com>
2025-11-30 22:21:56 -0500

    chore: Convert to a workspace

--- crates/rusm/src/lib.rs
+++ crates/rusm/src/lib.rs
@@ -1,5 +1,5 @@
 //! # RUst Service Modules (rusm)
-
+//!
 //! rusm is an opinionated paradigm for building service-like rust crates which can be composed together for many
 //! of the benefits of Service-oriented Architecture without the significant overhead of separately deployable microservices.
 //! rusm lets you build a bundle of modules which can be deployed together in a monolith at first and then pulled apart into
